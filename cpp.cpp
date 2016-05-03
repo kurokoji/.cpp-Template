6afbcada430e7c3be64203104833c4b6f7f26b65
kurokoji <shu.kakihana@gmail.com>
2016-05-03 14:25:16 +0900

    First commit

---
+++ cpp.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// #define int long long
+ 
+/* short */
+#define pb push_back
+#define print(x) cout << x << '\n'
+ 
+/* REPmacro */
+#define REPS(i, a, n) for (int i = (a); i < (n); i++)
+#define REP(i, n) for (int i = 0; i < (n); i++)
+#define RREP(i, n) for (int i = 1; i <= (n); i++)
+#define DEPS(i, a, n) for (int i = (a); i >= n; i--)
+#define DEP(i, n) for (int i = (n); i >= 0; i--)
+
+/* typedef */
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef vector<vector<int> > vii;
+typedef pair<int, int> pii;
+
+/* const */
+const int INF = INT_MAX;
+const int dx[] = {0, 1, 0, -1}, dy[] = {1, 0, -1, 0};
+
+/* func */
+inline bool is_uruu(int y) {return (y % 4 == 0 && (y % 100 != 0 || y % 400 == 0));}
+
+
+signed main()
+{
+  ios::sync_with_stdio(false);
+
+
+  return (0);
+}
