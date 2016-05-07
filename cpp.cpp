69fdab3eca768793c8ff52ce165472fac5a9df28
kurokoji <shu.kakihana@gmail.com>
2016-05-07 20:53:43 +0900

    vim折り畳み用コメントの追加

--- cpp.cpp
+++ cpp.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 // #define int long long
- 
+
+// cpp template {{{
+
 /* short */
 #define pb push_back
 #define print(x) cout << x << '\n'
@@ -30,6 +32,7 @@ const int dx[] = {0, 1, 0, -1}, dy[] = {1, 0, -1, 0};
 /* func */
 inline bool is_uruu(int y) {return (y % 4 == 0 && (y % 100 != 0 || y % 400 == 0));}
 
+// }}}
 
 signed main()
 {
