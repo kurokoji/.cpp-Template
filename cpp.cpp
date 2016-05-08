3b8442450e35f3031ba1e46507441c3ad263b608
kurokoji <shu.kakihana@gmail.com>
2016-05-08 12:33:10 +0900

    コメントアウトの仕方の変更

--- cpp.cpp
+++ cpp.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // #define int long long
 
-// cpp template {{{
+/* cpp template {{{ */
 
 /* short */
 #define pb push_back
@@ -33,7 +33,7 @@ const int dx[] = {0, 1, 0, -1}, dy[] = {1, 0, -1, 0};
 inline bool is_uruu(int y) {return (y % 4 == 0 && (y % 100 != 0 || y % 400 == 0));}
 inline bool is_inside(int y, int x, int H, int W) {return (y >= 0 && x >= 0 && y < H && x < W);}
 
-// }}}
+/* }}} */
 
 signed main()
 {
