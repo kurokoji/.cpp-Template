f3c3820a3bec7b21680162954466d2cbc60719e2
kurokoji <shu.kakihana@gmail.com>
2016-05-12 23:16:12 +0900

    is_insideからinsideに変更

--- cpp.cpp
+++ cpp.cpp
@@ -31,7 +31,7 @@ const int dx[] = {0, 1, 0, -1}, dy[] = {1, 0, -1, 0};
 
 /* func */
 inline bool is_uruu(int y) {return (y % 4 == 0 && (y % 100 != 0 || y % 400 == 0));}
-inline bool is_inside(int y, int x, int H, int W) {return (y >= 0 && x >= 0 && y < H && x < W);}
+inline bool inside(int y, int x, int H, int W) {return (y >= 0 && x >= 0 && y < H && x < W);}
 
 /* }}} */
 
