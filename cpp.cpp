6ee2b2e02f8f81b621f606903952e9bfaef20360
kurokoji <shu.kakihana@gmail.com>
2016-06-27 17:51:23 +0900

    returnの()を消した

--- cpp.cpp
+++ cpp.cpp
@@ -44,8 +44,8 @@ const double EPS = 1e-9;
 const int dx[] = {0, 1, 0, -1, 1, -1, 1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1};
 
 /* func */
-inline bool is_uruu(int y) {return (y % 4 == 0 && (y % 100 != 0 || y % 400 == 0));}
-inline bool inside(int y, int x, int H, int W) {return (y >= 0 && x >= 0 && y < H && x < W);}
+inline bool is_uruu(int y) {return y % 4 == 0 && y % 100 != 0 || y % 400 == 0;}
+inline bool inside(int y, int x, int H, int W) {return y >= 0 && x >= 0 && y < H && x < W;}
 
 /* }}} */
 
@@ -55,5 +55,5 @@ signed main()
   ios::sync_with_stdio(false);
 
 
-  return (0);
+  return 0;
 }
