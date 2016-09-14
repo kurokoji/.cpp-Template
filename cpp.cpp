43f50c0897eac9b5d35c79b9a35342ecac7ede7d
kurokoji <shu.kakihana@gmail.com>
2016-09-14 23:20:52 +0900

    名前空間つけた

--- cpp.cpp
+++ cpp.cpp
@@ -58,7 +58,7 @@ inline bool inside(int y, int x, int H, int W) {return y >= 0 && x >= 0 && y < H
 inline int in() {int x; std::cin >> x; return x;}
 template <typename T> void print(T x) {std::cout << x << '\n';}
 template <typename T>
-void print(std::vector<T>& v, string s = " ") {
+void print(std::vector<T>& v, std::string s = " ") {
   REP(i, v.size()) {
     if (i != 0) std::cout << s;
     std::cout << v[i];
