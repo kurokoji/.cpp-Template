9d31db27a8f56c31af470caa63474b726d5d2e2f
kurokoji <shu.kakihana@gmail.com>
2016-12-09 14:32:41 +0900

    色々追加

--- template/template.cpp
+++ template/template.cpp
@@ -8,6 +8,7 @@
  \______.'[___]|__][___][___||__]'.__.'[__;.__.'  '.__.'_/ 
 
 */
+// clang-format off
 /* }}} */
 
 #include <bits/stdc++.h>
@@ -20,19 +21,22 @@ struct Fast {Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
 
 /* short */
 #define pb push_back
+#define eb emplace_back
 #define mp make_pair
 #define Fi first
 #define Se second
-#define ALL(v) (v).begin(), (v).end()
+#define ALL(v) begin(v), end(v)
+#define RALL(v) rbegin(v), rend(v)
 #define X real()
 #define Y imag()
 
 /* REPmacro */
-#define REPS(i, a, n) for (int i = (a); i < (n); ++i)
+#define REPS(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
 #define REP(i, n) REPS(i, 0, n)
 #define RREP(i, n) REPS(i, 1, n + 1)
-#define DEPS(i, a, n) for (int i = (a); i >= n; --i)
+#define DEPS(i, a, n) for (ll i = (a); i >= (ll)(n); --i)
 #define DEP(i, n) DEPS(i, n, 0)
+#define EACH(i, n) for (auto&& i : n)
 
 /* debug */
 #define debug(x) cerr << x << " " << "(L:" << __LINE__ << ")" << '\n';
@@ -46,6 +50,9 @@ using vvvi = vector<vvi>;
 using pii = pair<int, int>;
 using D = double;
 using P = complex<D>;
+using vs = vector<string>;
+template <typename T> using PQ = priority_queue<T>;
+template <typename T> using minPQ = priority_queue<T, vector<T>, greater<T>>;
 
 /* const */
 const int INF = 1001001001;
@@ -56,21 +63,19 @@ const int dx[] = {0, 1, 0, -1, 1, -1, 1, -1}, dy[] = {1, 0, -1, 0, 1, -1, -1, 1}
 
 /* func */
 inline bool inside(int y, int x, int H, int W) {return y >= 0 && x >= 0 && y < H && x < W;}
-inline int in() {int x; std::cin >> x; return x;}
-template <typename T> void print(T x) {std::cout << x << '\n';}
-template <typename T>
-void print(std::vector<T>& v, std::string s = " ") {
-  REP(i, v.size()) {
-    if (i != 0) std::cout << s;
-    std::cout << v[i];
-  }
-  std::cout << '\n';
-}
-
+inline int in() {int x; cin >> x; return x;}
+inline ll IN() {ll x; cin >> x; return x;}
+inline vs split(const string& t, char c) {vs v; stringstream s(t); string b; while(getline(s, b, c)) v.eb(b); return v;}
+template <typename T> inline bool chmin(T& a, const T& b) {if (a > b) a = b; return a > b;}
+template <typename T> inline bool chmax(T& a, const T& b) {if (a < b) a = b; return a < b;}
+template <typename T, typename S> inline void print(const pair<T, S>& p) {cout << p.first << " " << p.second << endl;}
+template <typename T> inline void print(const T& x) {cout << x << '\n';}
+template <typename T, typename S> inline void print(const vector<pair<T, S>>& v) {for (auto&& p : v) print(p);}
+template <typename T> inline void print(const vector<T>& v, string s = " ") {REP(i, v.size()) cout << v[i] << (i != (ll)v.size() - 1 ? s : "\n");}
+// clang-format on
 /* }}} */
 
-signed main()
-{
+signed main() {
   <+CURSOR+>
 
   return 0;
